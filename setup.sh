acbda5c6fdf27af2c54fe8442a2e4b24a8382468
Anouar Saadi <56122025+a-saadi@users.noreply.github.com>
2021-04-01 17:41:12 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Nginx container
+# Nginx container 80:80 for http 443:443 for https
 docker build -t nginx:service srcs/Nginx
 
-# phpMyAdmin container
+# phpMyAdmin container 5000:5000
 docker build -t phpmyadmin:service srcs/phpMyAdmin
 
-# MySQL container
+# MySQL container 5050:5050
 docker build -t phpmyadmin:service srcs/MySQL
