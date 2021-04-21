a64fd08d45deaf23a2e56513991e7896d5607c59
Anouar Saadi <asaadi@e2r5p3.1337.ma>
2021-04-21 16:41:37 +0000

    Ft_services: FTPS...

--- setup.sh
+++ setup.sh
@@ -1,35 +1,37 @@
-#!/bin/bash
+#!/bin/sh
 
-# Set up the cluster
-# minikube start
-# eval $(minikube docker-env)
 
-# MetalLB Installation & Config.
+# Set up the cluster & MetalLB Installation & Config.
+A="setup"
+if [ $1 == $A ]; then
+    minikube start
+    eval $(minikube docker-env)
     kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
     kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
     kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
     kubectl apply -f srcs/MetalLB/ConfigMap.yaml
+fi;
 
-my_arr=( nginx phpmyadmin mysql wordpress )
+svc_arr=( nginx phpmyadmin mysql wordpress ftps)
 
 # Images building
-a="build"
-if [ $1 == $a ]; then
-    for i in "${my_arr[@]}";
+B="build"
+if [ $1 == $B ]; then
+    for i in "${svc_arr[@]}";
         do docker build -t $i srcs/$i;
     done;
 fi;
 
 # Create Deployemnts & Services
-b="apply"
-if [ $1 == $b ]; then
-    for i in "${my_arr[@]}";
+C="apply"
+if [ $1 == $C ]; then
+    for i in "${svc_arr[@]}";
         do kubectl apply -f ./srcs/$i/;
     done;
 fi;
 
 # Removin' all Deployments & Services
-c="rm"
-if [ $1 == $c ]; then 
-    for i in "${my_arr[@]}"; do kubectl delete -f ./srcs/$i/; done;
+D="rm"
+if [ $1 == $D ]; then 
+    for i in "${svc_arr[@]}"; do kubectl delete -f ./srcs/$i/; done;
 fi;
--- srcs/FTPS/srcs/run.sh
+++ srcs/FTPS/srcs/run.sh
@@ -2,3 +2,16 @@
 
 openrc 2> /dev/null || true
 touch /run/openrc/softlevel
+
+USER="asaadi"
+PASS="asaadi"
+
+addgroup -g 433 -S $USER
+adduser -u 431 -D -G $USER -h /home/$USER -s /bin/false  $USER
+
+echo "$USER:$PASS" | chpasswd
+chown $USER:$USER /home/$USER/ -R
+
+rc-service vsftpd start 2> /dev/null || true
+vsftpd
+# sh
