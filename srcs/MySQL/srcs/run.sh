d145042a0b103248db04de0b1721ac4479b11e4b
Anouar Saadi <56122025+a-saadi@users.noreply.github.com>
2021-10-30 22:16:49 +0100

    Update run.sh

--- srcs/MySQL/srcs/run.sh
+++ srcs/MySQL/srcs/run.sh
@@ -4,13 +4,12 @@
 openrc 2> /dev/null || true ; touch /run/openrc/softlevel; \
     /etc/init.d/mariadb setup 2> /dev/null || true; \
     service mariadb start 2> /dev/null || true; \
-    mysql -u root -e "CREATE DATABASE phpmyadmin";\
-    mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'pmauser'@'%' IDENTIFIED BY 'pmapass';";\
-    mysql -u root -e "FLUSH PRIVILEGES";\
-    mysql -u root -e "CREATE DATABASE wordpress";\
-    mysql -u root -e "CREATE USER 'wpuser'@'%' IDENTIFIED BY 'wppass';";\
-    mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'%';";\
-    mysql -u root -e "FLUSH PRIVILEGES";
+    mysql -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY 'root';";\
+    mysql -u root -proot -e "FLUSH PRIVILEGES";\
+    mysql -u root -proot -e "CREATE DATABASE wordpress";\
+    mysql -u root -proot -e "CREATE USER 'wpuser'@'%' IDENTIFIED BY 'wppass';";\
+    mysql -u root -proot -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'%';";\
+    mysql -u root -proot -e "FLUSH PRIVILEGES";
 service mariadb start 2> /dev/null || true
 
 # Add WordPress database table
